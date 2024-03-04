1850ca801c38ff50ebc598a63621cd80a527f423
João Luft <contato.joaoluft@gmail.com>
2024-03-04 06:25:11 -0300

    Feat: adicionado paginação nas questões, contador no header e funcionamento dos estados e passagem de dados

--- src/Components/Quiz/Button/index.js
+++ src/Components/Quiz/Button/index.js
@@ -1,6 +1,6 @@
-const Button = (props) => {
+const Button = ({text, click}) => {
     return(
-        <button className="px-24 py-6 rounded-full text-white bg-[#ed686e] font-semibold uppercase text-2xl tracking-widest transition-all hover:scale-105 hover:bg-[#cf595e]">{props.text}</button>
+        <button onClick={click} className="px-24 py-6 rounded-full text-white bg-[#ed686e] font-semibold uppercase text-2xl tracking-widest transition-all hover:scale-105 hover:bg-[#cf595e]">{text}</button>
     )
 }
 
